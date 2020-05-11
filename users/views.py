cf27d9cab7017bb0945c58b1280dc7cae6de486f
plyn85 <plyn99@gmail.com>
2020-05-11 19:27:20 +0100

    adding register form to users/forms.py-email field added

--- users/views.py
+++ users/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
-    
+        form = UserRegisterForm(request.POST)
+
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
-        
+
             messages.success(
                 request, f'{username} Your account has been created !')
             return redirect('fourm-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
 
-    return render(request,'users/register.html',{'form':form})
-
+    return render(request, 'users/register.html', {'form': form})
