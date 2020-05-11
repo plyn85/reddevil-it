5bd87d83227b6774e84b2a510ae5278510dad1f5
plyn85 <plyn99@gmail.com>
2020-05-11 18:55:12 +0100

    adding form validation logic to register view

--- users/views.py
+++ users/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+    
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+        
+            messages.success(
+                request, f'{username} Your account has been created !')
+            return redirect('fourm-home')
+    else:
+        form = UserCreationForm()
+
     return render(request,'users/register.html',{'form':form})
 
