4ce490bcbeb58410533e7a7ae56adf56d8b43022
plyn85 <plyn99@gmail.com>
2020-05-12 16:07:55 +0100

    adding profile view, html page an url path to users

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import register
+from .views import register, profile
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('profile/', profile, name="profile"),
     path('register/', register, name="register"),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
