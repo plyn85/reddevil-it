efff9c81cdebc43ea132f0c80b1c0343f80424ab
plyn85 <plyn99@gmail.com>
2020-05-13 20:43:45 +0100

    adding url path for user post list view

--- fourm/urls.py
+++ fourm/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserListView
 
 urlpatterns = [
     path('', PostListView.as_view(), name="fourm-home"),
+    path('user/<str:username>', UserPostListView.as_view(), name="user-post"),
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('post/new/', PostCreateView.as_view(), name="post-create"),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name="post-delete"),
