c5c5e7b6c336f8ba50f0628c137e68cda42b1ddc
plyn85 <plyn99@gmail.com>
2020-05-14 16:24:43 +0100

    adding password reset done url path an html page-users aapp

--- users/urls.py
+++ users/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
     path('password_reset/', auth_views.PasswordResetView.as_view(
         template_name='users/password_reset.html'), name="password_reset"),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'), name="password_reset_done"),
 
 ]
