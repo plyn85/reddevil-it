8f42d01e37bfd6b0d8a346f7bfced556fc23df1e
plyn85 <plyn99@gmail.com>
2020-05-15 16:53:03 +0100

    adding signals.py users app-profile now created when user is

--- users/models.py
+++ users/models.py
@@ -8,3 +8,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    # def save(self, *args, **kwargs):
+    #     super()
+    #     # now where going grab the saved Image an resize It
+    #     img = Image.open(self.image.path)
+
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
