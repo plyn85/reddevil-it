44f7bbc12958620ac6cb9b4879fbd360f8f4e197
plyn85 <plyn99@gmail.com>
2020-05-15 15:49:30 +0100

    adding media_url root to config/urls

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,16 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('fourm.urls')),
     path('', include('users.urls')),
 ]
+
+# this Is only being added when where in debug mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
