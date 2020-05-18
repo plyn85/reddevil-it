51bba3bd415761f1a4702283958a050273d15ad4
plyn85 <plyn99@gmail.com>
2020-05-18 10:36:41 +0100

    adding comment approved/not approved url paths to fourm/urls

--- fourm/urls.py
+++ fourm/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView, add_comment_to_post
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView, add_comment_to_post, comment_approve, comment_remove
 
 urlpatterns = [
     path('', PostListView.as_view(), name="fourm-home"),
@@ -10,5 +10,8 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
     path('post/<int:pk>/comment/', add_comment_to_post,
          name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/',
+         comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', comment_remove, name='comment_remove'),
 
 ]
