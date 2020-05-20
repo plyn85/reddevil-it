9756fb470f6d8785bf11808504944471e6cb76e3
plyn85 <plyn99@gmail.com>
2020-05-20 16:01:22 +0100

    adding filter_by_multiple_fields method- fourm/filters.py

--- fourm/filters.py
+++ fourm/filters.py
@@ -1,38 +1,41 @@
 import django_filters as filters
 from django_filters import CharFilter
 from .models import Post
+from django.db.models import Q
 
 
 class PostFilter(filters.FilterSet):
     """ taken and altered from a tutorial found at https://www.youtube.com/watch?v=nle3u6Ww6Xktaken """
 
-    # # setting custom lables
-    # title = filters.CharFilter(
-    #     label="Search Post Titles", lookup_expr='icontains')
-    # content = filters.CharFilter(
-    #     label="Search Post Content", lookup_expr='icontains')
-
     CHOICES = (
         ('ascending', 'Oldest Posts'),
         ('desending', 'Newest Posts'),
     )
-
+    # setting custom labels and filters
+    title = filters.CharFilter(
+        label="Search Post Titles", lookup_expr='icontains')
+    content = filters.CharFilter(
+        label="Search Post content", lookup_expr='icontains')
+    # author = filters.CharFilter(
+    #     label="Search Post author")
+    multi_name_fields = filters.CharFilter(
+        label="Search by post title, post content", method='filter_by_all_fields')
     ordering = filters.ChoiceFilter(
-        label='Search by Date Posted', choices=CHOICES, method='filter_by_order')
+        label='Search by Newest/Oldest', choices=CHOICES, method='filter_by_order')
+
+    class Meta:
+        model = Post
+        fields = [
+
+        ]
 
     def filter_by_order(self, queryset, name, value):
         expression = 'date_posted' if value == 'ascending' else '-date_posted'
         return queryset.order_by(expression)
 
-    # def __init__(self, *args, **kwargs):
-    #     super(PostFilter, self).__init__(*args, **kwargs)
-    #     self.filters['title'].label = "Article"
-    #     self.filters['content'].label = "Seller"
+    def filter_by_all_fields(self, queryset, name, value):
+        return Post.objects.filter(
+            Q(title__icontains=value) | Q(
+                content__icontains=value)
 
-    class Meta:
-        model = Post
-        fields = {
-
-            'title': ['icontains'],
-            'content': ['icontains'],
-        }
+        )
