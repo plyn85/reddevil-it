4441453630d1a15eaaeb09075e379f077ceff347
plyn85 <plyn99@gmail.com>
2020-05-21 18:12:25 +0100

    adding method to users/models.py tat resizes images when saved

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -16,11 +17,12 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-        #     super()
-        #     # now where going grab the saved Image an resize It
-        #     img = Image.open(self.image.path)
+    def save(self):
+        super().save()
 
-        #     if img.height > 300 or img.width > 300:
-        #         output_size = (300, 300)
-        #         img.thumbnail(output_size)
-        #         img.save(self.image.path)
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.height > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
