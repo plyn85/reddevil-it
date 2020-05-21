6f25de0236db1560157e4b4bba11892a548df410
plyn85 <plyn99@gmail.com>
2020-05-21 17:20:30 +0100

    changing view-users/veiws-user can now update profile

--- users/views.py
+++ users/views.py
@@ -31,9 +31,22 @@ def register(request):
 
 @login_required
 def profile(request):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        # both forms have to be valid to save the data
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(
+                request, f'{request.user.username} Your account has been Updated!')
+            return redirect('profile')
 
-    u_form = UserUpdateForm(instance=request.user)
-    p_form = ProfileUpdateForm(instance=request.user.profile)
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
     context = {
         'u_form': u_form,
         'p_form': p_form
