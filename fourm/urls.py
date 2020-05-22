b790ef1bcc17b42721a844c6b9337d6622bc4e45
plyn85 <plyn99@gmail.com>
2020-05-22 15:50:49 +0100

    adding link to like-toggle url to fourm/home,user_posts + post_detail/html

--- fourm/urls.py
+++ fourm/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # path('search/', search, name='search'),
     path('user/<str:username>', UserPostListView.as_view(), name="user-post"),
     path('post/<int:pk>/like/',
-         PostLikeToggle.as_view(), name="like-toogle"),
+         PostLikeToggle.as_view(), name="like-toggle"),
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('post/new/', PostCreateView.as_view(), name="post-create"),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name="post-delete"),
