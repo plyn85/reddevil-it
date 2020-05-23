55917deceb7a68800c04a8cd8dfd8284ba56bd15
plyn85 <plyn99@gmail.com>
2020-05-23 20:43:58 +0100

    setting up views in store app

--- store/views.py
+++ store/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def store(request):
+    context = {}
+    return render(request, 'store/store.html')
+
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html')
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html')
+
+
+def main(request):
+    context = {}
+    return render(request, 'store/main.html')
