f0f98f1c9033b7eaa63ce42180f90acee965b9ab
plyn85 <plyn99@gmail.com>
2020-05-24 17:35:57 +0100

    adding html template to store/shop.html

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import cart, checkout, shop
 
 urlpatterns = [
-    path('', shop, name='shop'),
+    path('shop/', shop, name='shop'),
     path('cart/', cart, name='cart'),
     path('checkout/', checkout, name='checkout'),
 
