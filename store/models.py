5b2c0d694b36888e3cb95437a416aaefaada03de
plyn85 <plyn99@gmail.com>
2020-05-25 12:18:12 +0100

    adding customer model to store/models.py

--- store/models.py
+++ store/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=254, default='')
+    name = models.CharField(max_length=254, null=True)
     description = models.TextField()
     price = models.FloatField()
     image = models.ImageField(upload_to='images')
 
     def __str__(self):
         return self.name
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=254, null="True")
+    email = models.CharField(max_length=254, null="True")
