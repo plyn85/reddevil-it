ef21ed67b382fb8fcaa854b20e71a0dd111cce45
plyn85 <plyn99@gmail.com>
2020-05-25 15:30:56 +0100

    adding odered items to cart view in store/views

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Product, Order, Customer
+from .models import Product, Order, Customer, OrderItem
 
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(Customer)
+admin.site.register(OrderItem)
--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Order
 
 
 def shop(request):
@@ -9,10 +9,24 @@ def shop(request):
 
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        #    getting the customer
+        customer = request.user.customer
+        # creating or getting the order item
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        # getting the items attached to the order
+        # getting all order items that have the order as the parent
+        items = order.orderitem_set.all()
+    # if user is not logged in return an empty list
+    else:
+        items = []
+
+    context = {'items': items}
     return render(request, 'store/cart.html')
 
 
 def checkout(request):
+
     context = {}
     return render(request, 'store/checkout.html')
