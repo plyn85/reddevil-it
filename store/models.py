899496d34b413f823664cedbad5709c4debc379b
plyn85 <plyn99@gmail.com>
2020-05-25 14:04:03 +0100

    adding oOrderItem model  to store/models

--- store/models.py
+++ store/models.py
@@ -30,3 +30,10 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
