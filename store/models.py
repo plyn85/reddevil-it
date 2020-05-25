c5b523dc086034ffb7a09a61e80a6acb1b4475b9
plyn85 <plyn99@gmail.com>
2020-05-25 13:43:36 +0100

    adding order model to store/models

--- store/models.py
+++ store/models.py
@@ -16,3 +16,17 @@ class Customer(models.Model):
     user = models.OneToOneField(User, models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=254, null="True")
     email = models.CharField(max_length=254, null="True")
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return str(self.id)
