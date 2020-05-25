ac6526eb8e59f971eb6a2d3688ff1a76245d2adb
plyn85 <plyn99@gmail.com>
2020-05-25 19:13:18 +0100

    adding cart total an quantity to store/views store/cart.html

--- store/views.py
+++ store/views.py
@@ -22,7 +22,7 @@ def cart(request):
     else:
         items = []
 
-    context = {'items': items}
+    context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 
