dc8421e73cb5ffb227fa9e547eb7b3dbfef80770
plyn85 <plyn99@gmail.com>
2020-05-25 20:22:25 +0100

    adding cart veiw python logic to checkout view in store/views

--- store/views.py
+++ store/views.py
@@ -29,6 +29,20 @@ def cart(request):
 
 
 def checkout(request):
+    if request.user.is_authenticated:
+        #    getting the customer
+        customer = request.user.customer
+        # creating or getting the order item
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        # getting the items attached to the order
+        # getting all order items that have the order as the parent
+        items = order.orderitem_set.all()
+    # if user is not logged in return an empty list
+    else:
+        items = []
+        # setting empty cart for users who are not logged In
+    order = {'get_cart_total': 0, 'get_cart_items': 0}
 
-    context = {}
-    return render(request, 'store/checkout.html')
+    context = {'items': items, 'order': order}
+    return render(request, 'store/checkout.html', context)
