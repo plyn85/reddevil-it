4541b07851a736c0e9a88f2a112776f607939e4e
plyn85 <plyn99@gmail.com>
2020-05-26 18:00:16 +0100

    Added-conditional if/else for logged in/non loggedIn user-cart.js

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
+  // click function for add to cat button
   $(".update-cart").click(function (e) {
+    //  setting data-product and data action buttons to variables
     let productId = this.dataset.product;
     let action = this.dataset.action;
     console.log("productId:", productId, "action:", action, user);
+    // conditional for non logged In user
+    if (user === "AnonymousUser") {
+      console.log("not logged In");
+    } else console.log("user is logged In");
   });
 });
