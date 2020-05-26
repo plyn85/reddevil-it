59c8f7dee9ae240047d20fb30d42ff3dc5d1db12
plyn85 <plyn99@gmail.com>
2020-05-26 16:28:33 +0100

    adding script tag to base page  with var-make user available on all pages

--- static/js/cart.js
+++ static/js/cart.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
   $(".update-cart").click(function (e) {
     let productId = this.dataset.product;
     let action = this.dataset.action;
-    console.log("productId:", productId, "action:", action);
+    console.log("productId:", productId, "action:", action, user);
   });
 });
