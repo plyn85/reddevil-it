2d59a99ecfd66bbe0c817c437d57009aaae3fb68
plyn85 <plyn99@gmail.com>
2020-06-02 14:29:44 +0100

    adding event listner that shows error message when card payment invalid

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -4,9 +4,12 @@
     CSS from here: 
     https://stripe.com/docs/stripe-js
 */
+/* getting stripe public and client secret key from base html where there set as json|script variables*/
 
 var stripe_public_key = $("#id_stripe_public_key").text().slice(1, -1);
+
 var client_secret = $("#id_client_secret").text().slice(1, -1);
+
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
 var style = {
@@ -26,3 +29,18 @@ var style = {
 };
 var card = elements.create("card", { style: style });
 card.mount("#card-element");
+
+// handle realtime errors
+card.addEventListener("change", function (event) {
+  var errorDiv = document.getElementById("card-errors");
+  if (event.error) {
+    var html = `<span class='icon'role='alert'>
+        <i class = 'fas fa-times'></i>
+    </span>
+    <span>${event.error.message}</span>
+    `;
+    $(errorDiv).html(html);
+  } else {
+    errorDiv.textContent = "";
+  }
+});
