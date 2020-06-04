c6bed67058879e764cb7d07a79ee1006e2010125
plyn85 <plyn99@gmail.com>
2020-06-04 18:51:29 +0100

    adding python logic for shipping an customer forms to store/views/checkout

--- store/views.py
+++ store/views.py
@@ -21,20 +21,41 @@ def cart(request):
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
-    # setting cuttent cart to imported cart_contents method
+    if request.method == 'POST':
+        shipping_form_data = {
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+
+        }
+        customer_form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+
+        }
+        shipping_form = ShippingForm(shipping_form_data)
+        customer_form = CustomerForm(customer_form_data)
+        if shipping_form.is_valid() and customer_form.is_valid():
+            shipping_order = shipping_form.save()
+            customer_order = customer_form.save()
+
+        # setting cuttent cart to imported cart_contents method
     current_cart = cart_contents(request)
     total = current_cart['cart_total']
     stripe_total = round(total * 100)
     stripe.api_key = stripe_secret_key
     intent = stripe.PaymentIntent.create(
         amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY
-    )
-    customer_form = CustomerForm()
-    shipping_form = ShippingForm()
+        currency=settings.STRIPE_CURRENCY)
+
     if not stripe_public_key:
         messages.warning(request, 'Stripe Public key Is Missing!')
-
+    customer_form = CustomerForm()
+    shipping_form = ShippingForm()
     context = {
         'customer_form': customer_form,
         'shipping_form': shipping_form,
