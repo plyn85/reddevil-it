965e550f1b18daeaf6f730a5c3f2a2062aeb725c
plyn85 <plyn99@gmail.com>
2020-06-05 21:25:35 +0100

    adding for loop to contexts.py which calculates number of cart items

--- store/contexts.py
+++ store/contexts.py
@@ -16,11 +16,13 @@ def cart_contents(request):
         cart_total = order.get_cart_total
     #  getting cart total items
         cartItems = order.get_cart_items
-    # if user is not logged in return an empty list
+    # if user is not logged in return cart cookie
     else:
-
-        cart = json.loads(request.COOKIES['cart'])
-
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        # if theres no cookie return an empty cart object
+        except:
+            cart = {}
         print('CART:', cart)
 
         items = []
@@ -28,6 +30,9 @@ def cart_contents(request):
         order = {'get_cart_total': 0, 'get_cart_items': 0}
         # setting empty Items for users who are not logged In
         cartItems = order['get_cart_items']
+    # looping trough cart object and returning items plus the quantity of the entire cart
+    for i in cart:
+        cartItems += cart[i]['quantity']
 
     products = Product.objects.all()
 
