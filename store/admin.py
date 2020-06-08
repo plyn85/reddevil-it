7090f612f440a50b72b8cf7496a2b88ea0573e67
plyn85 <plyn99@gmail.com>
2020-06-08 15:07:28 +0100

    adding custom fields to store admin

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,42 @@
 from django.contrib import admin
 from .models import Product, Order, OrderItem
 
-admin.site.register(Product)
-admin.site.register(Order)
 
-admin.site.register(OrderItem)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+
+        'name',
+        'description',
+        'price',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class OrderItemAdminInline(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ('product',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdminInline,)
+
+    readonly_fields = ('transaction_id', 'date_ordered')
+
+    fields = ("profile", 'transaction_id', 'date_ordered', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county',
+              )
+
+    list_display = ('transaction_id', 'date_ordered', 'full_name',
+                    )
+
+    ordering = ('-date_ordered',)
+
+
+admin.site.register(Order, OrderAdmin)
