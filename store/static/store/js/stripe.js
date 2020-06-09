ce4e68e9bcd2d457e120e247491d6fa9e47c9f9c
plyn85 <plyn99@gmail.com>
2020-06-09 20:23:07 +0100

    adding overlay on payment in checkout form

--- store/static/store/js/stripe.js
+++ store/static/store/js/stripe.js
@@ -8,10 +8,9 @@
 
 var stripePublicKey = $("#id_stripe_public_key").text().slice(1, -1);
 
+var stripePublicKey = $("#id_stripe_public_key").text().slice(1, -1);
 var clientSecret = $("#id_client_secret").text().slice(1, -1);
-
 var stripe = Stripe(stripePublicKey);
-
 var elements = stripe.elements();
 var style = {
   base: {
@@ -20,7 +19,7 @@ var style = {
     fontSmoothing: "antialiased",
     fontSize: "16px",
     "::placeholder": {
-      color: "#000000",
+      color: "#aab7c4",
     },
   },
   invalid: {
@@ -31,28 +30,31 @@ var style = {
 var card = elements.create("card", { style: style });
 card.mount("#card-element");
 
-// handle realtime errors
+// Handle realtime validation errors on the card element
 card.addEventListener("change", function (event) {
   var errorDiv = document.getElementById("card-errors");
   if (event.error) {
-    var html = `<span class='icon'role='alert'>
-        <i class = 'fas fa-times'></i>
-    </span>
-    <span>${event.error.message}</span>
-    `;
+    var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
     $(errorDiv).html(html);
   } else {
     errorDiv.textContent = "";
   }
 });
 
-/// Handle form submit
+// Handle form submit
 var form = document.getElementById("payment-form");
 
 form.addEventListener("submit", function (ev) {
   ev.preventDefault();
   card.update({ disabled: true });
   $("#submit-button").attr("disabled", true);
+  $("#payment-form").fadeToggle(100);
+  $("#loading-overlay").fadeToggle(100);
   stripe
     .confirmCardPayment(clientSecret, {
       payment_method: {
@@ -68,6 +70,8 @@ form.addEventListener("submit", function (ev) {
                 </span>
                 <span>${result.error.message}</span>`;
         $(errorDiv).html(html);
+        $("#payment-form").fadeToggle(100);
+        $("#loading-overlay").fadeToggle(100);
         card.update({ disabled: false });
         $("#submit-button").attr("disabled", false);
       } else {
