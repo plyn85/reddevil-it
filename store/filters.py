89dc48a8f5cabd19ac79e72503207620b733b537
plyn85 <plyn99@gmail.com>
2020-06-15 21:33:25 +0100

    adding filter search bars to shop.html to search products

--- store/filters.py
+++ store/filters.py
@@ -5,11 +5,17 @@ from django import forms
 
 
 class ProductFilter(filters.FilterSet):
+    LOWEST_HIGHEST_PRICES = (
+        ('ascending', 'Lowest Price'),
+        ('desending', 'Highest Price'),
+    )
 
     name = filters.CharFilter(lookup_expr='icontains',
                               widget=forms.TextInput(
                                   attrs={'placeholder': 'Search Products'}),
                               )
+    lowest_highest_prices = filters.ChoiceFilter(empty_label="Sort By",
+                                                 choices=LOWEST_HIGHEST_PRICES, method='filter_by_price')
 
     class Meta:
         model = Product
@@ -18,3 +24,7 @@ class ProductFilter(filters.FilterSet):
 
 
         ]
+
+    def filter_by_price(self, queryset, name, value):
+        expression = 'price' if value == 'ascending' else '-price'
+        return queryset.order_by(expression)
