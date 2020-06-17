67659f36bbb153f701412840207ff1b55af5400b
plyn85 <plyn99@gmail.com>
2020-06-17 16:45:27 +0100

    password reset pages now styled and fully functional

--- users/urls.py
+++ users/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
         template_name='users/password_reset_comfirm.html'), name="password_reset_confirm"),
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='users/password_reset_complete.html'), name="password_reset-compelete"),
+        template_name='users/password_reset_complete.html'), name="password_reset_complete"),
     path('order_history/<transaction_id>',
          order_history, name='order_history'),
 
