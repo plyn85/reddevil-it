afcd85c2ec9c2fa19909da02f49cabab0a41d506
plyn85 <plyn99@gmail.com>
2020-06-24 15:21:35 +0100

    adding jqurey to show/hide search bars on mobile

--- store/static/store/js/cart.js
+++ store/static/store/js/cart.js
@@ -1,36 +1,34 @@
-$(document).ready(function () {
-  // click function for add to cart button
-  $(".update-cart").click(function () {
-    //  setting data-product and data action buttons from ckeckout.html to variables
-    let productId = this.dataset.product;
-    let action = this.dataset.action;
-    if (action == "add") {
-    }
-    addCookieItem(productId, action);
-  });
-  /*cookie created in script tag in base html 
+// click function for add to cart button
+$(".update-cart").click(function () {
+  //  setting data-product and data action buttons from ckeckout.html to variables
+  let productId = this.dataset.product;
+  let action = this.dataset.action;
+  if (action == "add") {
+  }
+  addCookieItem(productId, action);
+});
+/*cookie created in script tag in base html 
   adds an item to the cookie in the cart 
   if it those not exist if the item already 
   exists increase quantity by one  */
-  function addCookieItem(productId, action) {
-    console.log("not logged In");
-    if (action == "add") {
-      if (cart[productId] == undefined) {
-        cart[productId] = { quantity: 1 };
-      } else {
-        cart[productId]["quantity"] += 1;
-      }
+function addCookieItem(productId, action) {
+  console.log("not logged In");
+  if (action == "add") {
+    if (cart[productId] == undefined) {
+      cart[productId] = { quantity: 1 };
+    } else {
+      cart[productId]["quantity"] += 1;
     }
-    /* decrease item by one or if its equal or less then zero remove */
-    if (action == "remove") {
-      cart[productId]["quantity"] -= 1;
-      if (cart[productId]["quantity"] <= 0) {
-        console.log("item deleted");
-        delete cart[productId];
-      }
-    }
-
-    document.cookie = "cart=" + JSON.stringify(cart) + ";domain=;path=/";
-    location.reload();
   }
-});
+  /* decrease item by one or if its equal or less then zero remove */
+  if (action == "remove") {
+    cart[productId]["quantity"] -= 1;
+    if (cart[productId]["quantity"] <= 0) {
+      console.log("item deleted");
+      delete cart[productId];
+    }
+  }
+
+  document.cookie = "cart=" + JSON.stringify(cart) + ";domain=;path=/";
+  location.reload();
+}
