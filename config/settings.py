4e2940b290f0fbe5bd4eda937d8a5027d5bba2a1
plyn85 <plyn99@gmail.com>
2020-06-30 17:29:23 +0100

    altering database settimgs in settings heroku migtrate attempt

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ if os.path.exists("env.py"):
 import django_heroku
 import dj_database_url
 
-if os.environ.get('DEVELOPMENT'):
+if os.path.exists('DEVELOPMENT'):
     development = True
 else:
     development = False
@@ -85,14 +85,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 if development:
-    DATABASES = {
+        DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-    # else:
-# DATABASES = {'default': dj_database_url.parse(os.environ.get(('DATABASE_URL'))}
+else:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
