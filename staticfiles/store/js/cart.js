d3a3d30daa2dba0644a0fca102a6ca8f0cc61b3a
plyn85 <plyn99@gmail.com>
2020-07-01 12:58:10 +0100

    storing static files for heroku deployment

---
+++ staticfiles/store/js/cart.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function () {
+
+  
+$(".update-cart").click(function () {
+  
+  // setting variables
+  
+  let productId = this.dataset.product;
+  let action = this.dataset.action;
+  
+  addCookieItem(productId, action);
+});
+
+/*
+
+  cookie created in script tag in base html 
+  adds an item to the cookie in the cart 
+  if it those not exist if the item already 
+  exists increase quantity by one  
+  
+  */
+function addCookieItem(productId, action) {
+  console.log("not logged In");
+  if (action == "add") {
+    alert("You added one Item to your cart!");
+    if (cart[productId] == undefined) {
+      cart[productId] = { quantity: 1 };
+    } else {
+      cart[productId]["quantity"] += 1;
+    }
+  }
+  
+  /* decrease item by one or if its equal or 
+  less then zero remove */
+  if (action == "remove") {
+    alert("You deleted  one Item to your cart!");
+    cart[productId]["quantity"] -= 1;
+    if (cart[productId]["quantity"] <= 0) {
+      console.log("item deleted");
+      delete cart[productId];
+    }
+  }
+
+   document.cookie = "cart=" + JSON.stringify(cart) + ";domain=;path=/";
+   location.reload();
+}
+
+})
