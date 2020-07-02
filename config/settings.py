e90e7fe58ec9ad2c531ba8ce434442f8a8128e2c
plyn85 <plyn99@gmail.com>
2020-07-02 13:15:27 +0100

    changes to default file storage var im settings

--- config/settings.py
+++ config/settings.py
@@ -180,7 +180,7 @@ if 'USE_AWS' in os.environ:
    # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3BotoStorage'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
  # Override static and media URLs in production
